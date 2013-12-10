920456d07d2b1004ac1424721cdd7df7b64519ec
Taahir Ahmed <ahmed.taahir@gmailcom>
2013-12-10 07:06:40 -0600

    Forgot to add the statistics singleton.

---
+++ Assets/CrowdStatisticsSingleton.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+    
+}
+
+function Update ()
+{
+    // Find all crowd members.
+    var crowd_members = GameObject.FindGameObjectsWithTag("crowd-member");
+
+    var num_held = 0;
+    for(person in crowd_members)
+    {
+        if(person.transform.position.z > -5.6)
+            num_held = num_held + 1;
+    }
+
+    guiText.text = num_held.ToString();
+}
